5bcc6be3d1200b7e9eb58316c70bfcea0bb04ef1
wangdanzhen <1029035741@qq.com>
2021-01-26 22:44:02 +0800

    one version

---
+++ src/GenerateCode.java
@@ -0,0 +1,8 @@
+public class GenerateCode {
+
+	@SuppressWarnings("static-access")
+	public static void main(String[] args) throws Exception {
+		AutoGenerateCode autoGenerateCode=new AutoGenerateCode();
+		autoGenerateCode.main(null);
+	}
+}
